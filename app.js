79965e91935f7388b8d4dde15982bd197c9de39d
Abdullah-Nasir-WD <abdullahbnasir@gmail.com>
2024-12-01 19:40:28 +0500

    completed

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log("Hello World!");
+
+var sec = document.getElementById("seconds")
+var minute = document.getElementById("minute")
+var hours = document.getElementById("hours")
+
+var seconds;
+
+function start() {
+    seconds = setInterval(() => {
+       sec.innerHTML++
+       
+       if (seconds.innerHTML == 60) {
+            sec.innerHTML = "00"
+            minute.innerHTML++
+       }
+
+       if (minute.innerHTML == 60) {
+            minute.innerHTML = "00"
+            hours.innerHTML++
+       }
+    }, 1000);
+}
+
+function stop() {
+    clearInterval(seconds);
+}
+
+function reset() {
+
+    sec.innerHTML = "00"
+    minute.innerHTML = "00"
+    hours.innerHTML = "00"
+    clearInterval(seconds)
+
+}
